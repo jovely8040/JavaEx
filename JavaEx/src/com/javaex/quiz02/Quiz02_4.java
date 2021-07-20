54e4fd0563dbc6eb635d9866ea6a1f0bacd18005
조은혜 <jovely8040@gmail.com>
2021-07-20 11:05:10 +0900

    조건 분기문

--- JavaEx/src/com/javaex/quiz02/Quiz02_4.java
+++ JavaEx/src/com/javaex/quiz02/Quiz02_4.java
@@ -4,7 +4,7 @@ public class Quiz02_4 {
 	public static void main(String[] args) {
 		char ch = 'A';
 		
-		//	'A': 65, 'a': 97
+		//	'A': 65, 'a': 97 (*대문자와 소문자 차이는 항상 32)
 		char conv = (char)(ch + 32);
 		System.out.println(conv);
 	}
