0a2c439d9dacc6e6670da04309691574db4ad032
조은혜 <jovely8040@gmail.com>
2021-07-22 14:12:38 +0900

    필드, 접근제한자, 메서드

---
+++ JavaEx/src/com/javaex/oop/goods/v1/Goods.java
@@ -0,0 +1,10 @@
+package com.javaex.oop.goods.v1;
+
+public class Goods {
+// v1. 필드: 클래스 내부에서 사용할 변수
+	// 필드 선언
+	String name;
+	int price;
+	
+
+}
---
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -0,0 +1,10 @@
+package com.javaex.oop.goods.v2;
+// v2. 접근 제한자
+public class Goods {
+// v1. 필드: 클래스 내부에서 사용할 변수
+	// 필드 선언
+	private String name;
+	private int price;
+	
+
+}
