4718ed5bc2f3c0db772c260c436ddedc423d0153
조은혜 <jovely8040@gmail.com>
2021-07-26 16:16:45 +0900

    Java Lang API: Object

---
+++ JavaEx/src/com/javaex/api/objectclass/v2/Point.java
@@ -0,0 +1,32 @@
+package com.javaex.api.objectclass.v2;
+// v1. 모든 클래스의 최상의 부모는 Object이다
+// 		toString: 객체를 문자열로 출력할 때 호출되는 메서드
+//public class Point extends java.lang.object {
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		//	객체 출력 포맷을 리턴
+		return String.format(
+				"Point(%d, %d)", x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 전달받은 obj가 Point클래스의 객체?
+		if (obj instanceof Point) {
+			Point other = (Point)obj;
+			return x == other.x && y == other.y;
+		}
+		return super.equals(obj);
+	}
+	
+	
+}
